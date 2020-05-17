958561459a3003556ba380027187f76e905bd7b8
Oliver Altergott <altergott.oliver@gmail.com>
2020-05-17 15:50:20 +0200

    added files

---
+++ main/main.cpp
@@ -0,0 +1,27 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+
+extern "C"
+{
+#ifdef ESP_PLATFORM
+void app_main()
+{
+   
+}
+#else
+    int main(int /*argc*/, char** /*argv*/)
+    {
+        //App app{};
+        //app.start();
+        return 0;
+    }
+#endif
+    
+}
